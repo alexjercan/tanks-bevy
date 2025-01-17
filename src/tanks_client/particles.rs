62cbf03eeaae671c17339b008d019d2e38f275b3
Alex Jercan <jercan_alex27@yahoo.com>
2025-01-17 23:43:25 +0200

    more stuff

--- src/tanks_client/particles.rs
+++ src/tanks_client/particles.rs
@@ -14,6 +14,7 @@ pub mod prelude {
 
 #[derive(Resource)]
 struct ParticleSystems {
+    impact: Handle<EffectAsset>,
     explosion: Handle<EffectAsset>,
     smoke: Handle<EffectAsset>,
 }
@@ -40,6 +41,104 @@ fn setup(
     mut effects: ResMut<Assets<EffectAsset>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
+    // Small explosion
+    let mut color_gradient1 = Gradient::new();
+    color_gradient1.add_key(0.0, Vec4::new(4.0, 4.0, 4.0, 1.0));
+    color_gradient1.add_key(0.1, Vec4::new(4.0, 4.0, 0.0, 1.0));
+    color_gradient1.add_key(0.6, Vec4::new(4.0, 0.0, 0.0, 1.0));
+    color_gradient1.add_key(1.0, Vec4::new(4.0, 0.0, 0.0, 0.0));
+
+    let mut size_gradient1 = Gradient::new();
+    size_gradient1.add_key(0.0, Vec3::splat(0.05));
+    size_gradient1.add_key(0.3, Vec3::splat(0.05));
+    size_gradient1.add_key(1.0, Vec3::splat(0.0));
+
+    let writer = ExprWriter::new();
+
+    // Give a bit of variation by randomizing the age per particle. This will
+    // control the starting color and starting size of particles.
+    let age = writer.lit(0.).uniform(writer.lit(0.2)).expr();
+    let init_age = SetAttributeModifier::new(Attribute::AGE, age);
+
+    // Give a bit of variation by randomizing the lifetime per particle
+    let lifetime = writer.lit(0.8).normal(writer.lit(1.2)).expr();
+    let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
+
+    // Add constant downward acceleration to simulate gravity
+    let accel = writer.lit(Vec3::Y * -16.).expr();
+    let update_accel = AccelModifier::new(accel);
+
+    // Add drag to make particles slow down a bit after the initial explosion
+    let drag = writer.lit(4.).expr();
+    let update_drag = LinearDragModifier::new(drag);
+
+    let init_pos = SetPositionSphereModifier {
+        center: writer.lit(Vec3::ZERO).expr(),
+        radius: writer.lit(0.1).expr(),
+        dimension: ShapeDimension::Volume,
+    };
+
+    // Give a bit of variation by randomizing the initial speed
+    let init_vel = SetVelocitySphereModifier {
+        center: writer.lit(Vec3::ZERO).expr(),
+        speed: (writer.rand(ScalarType::Float) * writer.lit(2.) + writer.lit(6.)).expr(),
+    };
+
+    // Clear the trail velocity so trail particles just stay in place as they fade
+    // away
+    let init_vel_trail =
+        SetAttributeModifier::new(Attribute::VELOCITY, writer.lit(Vec3::ZERO).expr());
+
+    let lead = ParticleGroupSet::single(0);
+    let trail = ParticleGroupSet::single(1);
+
+    let effect = EffectAsset::new(
+        // 2k lead particles, with 32 trail particles each
+        2048,
+        Spawner::once(2048.0.into(), true),
+        writer.finish(),
+    )
+    // Tie together trail particles to make arcs. This way we don't need a lot of them, yet there's
+    // a continuity between them.
+    .with_ribbons(2048 * 32, 1.0 / 64.0, 0.2, 0)
+    .with_name("Explosion")
+    .init_groups(init_pos, lead)
+    .init_groups(init_vel, lead)
+    .init_groups(init_age, lead)
+    .init_groups(init_lifetime, lead)
+    .init_groups(init_vel_trail, trail)
+    .update_groups(update_drag, lead)
+    .update_groups(update_accel, lead)
+    .render_groups(
+        ColorOverLifetimeModifier {
+            gradient: color_gradient1.clone(),
+        },
+        lead,
+    )
+    .render_groups(
+        SizeOverLifetimeModifier {
+            gradient: size_gradient1.clone(),
+            screen_space_size: false,
+        },
+        lead,
+    )
+    .render_groups(
+        ColorOverLifetimeModifier {
+            gradient: color_gradient1,
+        },
+        trail,
+    )
+    .render_groups(
+        SizeOverLifetimeModifier {
+            gradient: size_gradient1,
+            screen_space_size: false,
+        },
+        trail,
+    );
+
+    let impact_handle = effects.add(effect);
+
+    // Big explosion
     let mut color_gradient1 = Gradient::new();
     color_gradient1.add_key(0.0, Vec4::new(4.0, 4.0, 4.0, 1.0));
     color_gradient1.add_key(0.1, Vec4::new(4.0, 4.0, 0.0, 1.0));
@@ -202,6 +301,7 @@ fn setup(
     let smoke_handle = effects.add(effect);
 
     commands.insert_resource(ParticleSystems {
+        impact: impact_handle,
         explosion: explosion_handle,
         smoke: smoke_handle,
     });
@@ -232,11 +332,21 @@ fn play_cannon_fired(
 }
 
 fn play_shell_impact(
+    mut commands: Commands,
     mut impacts: EventReader<ShellImpactEvent>,
-    _particle_systems: Res<ParticleSystems>,
+    particle_systems: Res<ParticleSystems>,
 ) {
-    for _ in impacts.read() {
-        // TODO
+    for event in impacts.read() {
+        commands.spawn((
+            Name::new("Impact"),
+            ParticleEffectBundle {
+                effect: ParticleEffect::new(particle_systems.impact.clone()),
+                transform: Transform::from_translation(**event),
+                ..Default::default()
+            },
+            DespawnAfter::new(2.0),
+            StateScoped(GameStates::Playing),
+        ));
     }
 }
 
